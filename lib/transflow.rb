37f2bdaabf795142ce9606ecb1ec35c9ad570643
Piotr Solnica <piotr.solnica@gmail.com>
2015-08-16 12:53:29 +0200

    First passing spec :boom: :tada: :boom:

--- lib/transflow.rb
+++ lib/transflow.rb
@@ -1,5 +1,57 @@
-require "transflow/version"
+require 'transproc'
+require 'transflow/version'
 
 module Transflow
-  # Your code goes here...
+  class StepDSL
+    attr_reader :name
+
+    attr_reader :handler
+
+    attr_reader :container
+
+    attr_reader :steps
+
+    def initialize(name, options, container, steps, &block)
+      @name = name
+      @handler = options.fetch(:with)
+      @container = container
+      @steps = steps
+      instance_exec(&block) if block
+    end
+
+    def step(*args, &block)
+      self.class.new(*args, container, steps, &block).call
+    end
+
+    def call
+      steps << [name, container[handler]]
+    end
+  end
+
+  class FlowDSL
+    attr_reader :options
+
+    attr_reader :container
+
+    attr_reader :steps
+
+    def initialize(options, &block)
+      @options = options
+      @container = options.fetch(:container)
+      @steps = []
+      instance_exec(&block)
+    end
+
+    def step(*args, &block)
+      StepDSL.new(*args, container, steps, &block).call
+    end
+
+    def call
+      steps.map(&:last).reverse.reduce(:>>)
+    end
+  end
+end
+
+def Transflow(options = {}, &block)
+  Transflow::FlowDSL.new(options, &block).call
 end
--- spec/transflow_spec.rb
+++ spec/transflow_spec.rb
@@ -1,11 +1,48 @@
-require 'spec_helper'
-
-describe Transflow do
-  it 'has a version number' do
-    expect(Transflow::VERSION).not_to be nil
+RSpec.describe Transflow do
+  let(:transflow) do
+    Transflow(container: fns) do
+      step :preprocess, with: :preprocess_input do
+        step :validate, with: :validate_input do
+          step :persist, with: :persist_input
+        end
+      end
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  let(:fns) do
+    Module.new do
+      extend Transproc::Registry
+
+      import :symbolize_keys, from: Transproc::HashTransformations
+
+      def self.preprocess_input(input)
+        t(:symbolize_keys)[input]
+      end
+
+      def self.validate_input(input)
+        raise 'email nil' if input[:email].nil?
+        input
+      end
+
+      def self.persist_input(input)
+        DB << input
+      end
+
+      def self.handle_validation_error(error)
+        ERRORS << error.message
+      end
+    end
+  end
+
+  before do
+    DB = []
+  end
+
+  it 'allows defining a business transaction flow' do
+    input = { 'name' => 'Jane', email: 'jane@doe.org' }
+
+    transflow[input]
+
+    expect(DB).to include(name: 'Jane', email: 'jane@doe.org')
   end
 end
