3e840ea47f7d09a11681ed6dfa713386b64b4df1
Piotr Solnica <piotr.solnica@gmail.com>
2015-08-17 13:03:40 +0200

    Run kw args spec for > 2.0

--- spec/unit/transaction_spec.rb
+++ spec/unit/transaction_spec.rb
@@ -34,13 +34,15 @@ RSpec.describe Transflow::Transaction do
       end
     end
 
-    context 'with steps accepting a kw args' do
-      let(:step1) { -> i { { i: i, j: i + 1 } } }
-      let(:step2) { -> i:, j: { i + j } }
-      let(:step3) { -> i { i + 3 } }
+    if RUBY_VERSION > '2.0'
+      context 'with steps accepting a kw args' do
+        let(:step1) { -> i { { i: i, j: i + 1 } } }
+        let(:step2) { -> i:, j: { i + j } }
+        let(:step3) { -> i { i + 3 } }
 
-      it 'composes steps and calls them' do
-        expect(transaction[1]).to be(6)
+        it 'composes steps and calls them' do
+          expect(transaction[1]).to be(6)
+        end
       end
     end
 
